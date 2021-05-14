3538ff406cf1e12cd4c26ffca9a5078e53266c44
urielcaire <uriel.caire@gmail.com>
2021-05-13 23:01:47 -0300

    Import time-series data

--- time-series/intro-time-series.R
+++ time-series/intro-time-series.R
@@ -11,3 +11,10 @@ myts <- ts(myts, start = c(2012,1), end=c(2016,12), frequency = 12)
 myts
 class(myts)
 plot(myts)
+
+# importing time series data
+#tempts = read.csv(file.choose(), sep = ",", header = F)
+tempts = read.csv("time-series/support-material/Dados/importar.csv", sep = ",",
+                  header = F)
+tempts = ts(tempts[2], start = c(1884), end=c(1939), frequency = 1)
+plot(tempts)
